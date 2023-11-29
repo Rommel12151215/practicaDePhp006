7649e7b52ebe2c257cc6ff132af97ac2aaa82066
Rommel <karlsrommel255@gmail.com>
2023-11-29 17:42:38 -0600

    practicaDePhp006

---
+++ hola.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Hola Mundo</h1>
+
+<p><a href="index.php">Volver a la página principal</a></p>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Bienvenido a mi sitio web</h1>
+
+<p><a href="hola.php">Haz clic aquí para ver un mensaje</a></p>
+
+</body>
+</html>
